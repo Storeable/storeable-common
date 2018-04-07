7a0391f43b49f07660721290a481cba73b677098
Vincent Chavez <vchavez92780@gmail.com>
2018-04-06 20:42:22 -0400

    Added server side utils

---
+++ src/helpers/server/utils.js
@@ -0,0 +1,23 @@
+import crypto from 'crypto';
+
+/**
+ * Returns a validation error object used by Sequelize.
+ * @param {String} attr Attribute name
+ * @param {String} message Error message
+ * @returns {Object}
+ */
+export const buildSequelizeValidationError = (attr, message) => ({
+  errors: [
+    {
+      path: attr,
+      message
+    }
+  ]
+});
+
+/**
+ * Encrypts the specified value.
+ * @param {String} value
+ * @returns {String}
+ */
+export const encryptString = value => crypto.createHash('sha1').update(value).digest('base64');
