e8ab448624a921df2113daee2c9b88d46bc204b8
Devin Encalada <devinencalada@gmail.com>
2018-05-10 11:45:59 -0400

    added two more constants for user

---
+++ src/constants/userStatus.js
@@ -0,0 +1,15 @@
+export const INACTIVE = 1;
+export const ACTIVE = 2;
+export const SUSPENDED = 3;
+
+export const userStatuses = {
+  1: 'Inactive',
+  2: 'Active',
+  3: 'Suspended'
+};
+
+export const selectUserStatuses = [
+  { text: 'Inactive', value: 1 },
+  { text: 'Active', value: 2 },
+  { text: 'Suspended', value: 3 }
+];
---
+++ src/constants/userType.js
@@ -0,0 +1,9 @@
+export const userTypes = {
+  1: 'Admin',
+  2: 'Customer'
+};
+
+export const selectUserTypes = [
+  { text: 'Admin', value: 1 },
+  { text: 'Customer', value: 2 }
+];
