66fb03607927cb7913c4bbc11d00f6b779e7540b
Devin Encalada <devinencalada@gmail.com>
2018-05-17 15:30:52 -0400

    added a new reservation constant

---
+++ src/constants/reservationStatus.js
@@ -0,0 +1,15 @@
+export const PENDING = 1;
+export const ACCEPTED = 2;
+export const DECLINED = 3;
+
+export const listingStatuses = {
+  1: 'Pending',
+  2: 'Accepted',
+  3: 'Declined'
+};
+
+export const selectListingStatuses = [
+  { text: 'Pending', value: 1 },
+  { text: 'Accepted', value: 2 },
+  { text: 'Declined', value: 3 }
+];
