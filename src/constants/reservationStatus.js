51547892426a50f4098cfb5e457cafcad295af5e
Devin Encalada <devinencalada@gmail.com>
2018-05-17 15:32:43 -0400

    fixed constant wording error

--- src/constants/reservationStatus.js
+++ src/constants/reservationStatus.js
@@ -2,13 +2,13 @@ export const PENDING = 1;
 export const ACCEPTED = 2;
 export const DECLINED = 3;
 
-export const listingStatuses = {
+export const reservationStatuses = {
   1: 'Pending',
   2: 'Accepted',
   3: 'Declined'
 };
 
-export const selectListingStatuses = [
+export const selectReservationStatuses = [
   { text: 'Pending', value: 1 },
   { text: 'Accepted', value: 2 },
   { text: 'Declined', value: 3 }
